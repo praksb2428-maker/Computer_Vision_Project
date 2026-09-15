2d0bebe003b87340be02ccdf2bc1a9b0fe662b25
praksb2428-maker <praksb2428@gmail.com>
2026-09-15 13:15:08 +0900

    Update 2-6_python.py

--- ch2/2-6_python.py
+++ ch2/2-6_python.py
@@ -5,9 +5,10 @@ img = cv.imread('ch2/girl_laughing.jpg')
 
 if img is None:
     sys.exit("Could not read the image.")
-
+# 네모 박스
 cv.rectangle(img, (830, 30), (1000, 200), (0, 0, 255), 2)
-cv.putText(img, 'blue', (830, 24), cv.FONT_HERSHEY_SIMPLEX, 1, (250, 0, 0), 2)
+# 글자
+cv.putText(img, 'blue', (830, 24), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
 cv.imshow('Draw', img)
 cv.waitKey()
